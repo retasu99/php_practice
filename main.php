bb101de32cfea3ff259d60e1bd174732b19b6cd6
retasu99 <182311959+retasu99@users.noreply.github.com>
2025-03-29 12:51:28 +0900

    battle_sys_installed

--- main.php
+++ main.php
@@ -12,8 +12,37 @@
     $kamoshida -> name = "カモシダ";
     $goblin -> name = "ゴブリン";
 
-// =====================================================
+    $turn = 1;
 
-    echo "{$kamoshida -> name} \n";
-    echo "{$goblin -> name} \n";
+// 戦闘開始==============================================
+
+    echo "戦闘開始 \n";
+
+    while (($kamoshida -> hitPoint > 0) && ($goblin -> hitPoint > 0)) {
+
+//　各キャラのステータスを表示=====================================================
+
+        echo "～{$turn}ターン目～ \n";
+
+        echo "{$kamoshida -> name} : {$kamoshida -> hitPoint} / ".$kamoshida::MAX_HITPOINT."\n";
+        echo $goblin -> name." : ".$goblin -> hitPoint." / ".$goblin::MAX_HITPOINT."\n";
+        echo "\n";
+
+// 攻撃====================================================
+
+        $kamoshida -> doAttack($goblin);
+        echo "\n";
+        $goblin -> doAttack($kamoshida);
+        echo "\n";
+
+        $turn++;
+    };
+
+// 戦闘終了================================================
+
+    echo "戦闘終了 \n";
+    echo "{$kamoshida -> name} : {$kamoshida -> hitPoint} / ".$kamoshida::MAX_HITPOINT."\n";
+    echo $goblin -> name." : ".$goblin -> hitPoint." / ".$goblin::MAX_HITPOINT."\n";
+    echo "\n";
+
 ?>
